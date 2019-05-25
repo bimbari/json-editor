551d0f9f913287468dd655eae349669364db5311
Endri Bimbari <endribimbari@gmail.com>
2019-05-25 02:40:51 +0200

    Production changes

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist/json-editor')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/json-editor/index.html'));
+});
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on port ${port}`));
